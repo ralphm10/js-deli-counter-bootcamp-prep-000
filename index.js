7c768c849945271b2407c4e0980f80bc6003140f
Ralph Mallett <ralph_mallett@hotmail.com>
2019-09-21 15:11:58 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function takeANumber (katzDeli, name) {
+  
+  katzDeli.push(name);
+  return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+ }
+
+function nowServing (katzDeli) {
+  
+  if (katzDeli.length > 0) {return `Currently serving ${katzDeli.shift()}.`;
+  
+  
+  } else {return "There is nobody waiting to be served!"}
+  }
+  
+function currentLine (katzDeli)
+
+{
+  var newarray =[];
+for (let i = 0; i < katzDeli.length; i++)
+newarray.push(i + 1);
+return `The line is currently: ${newarray[i]} + ${katzDeli[i]}`;
+    }
